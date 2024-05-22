09c2c7f1dbbc1ddf91cc718673b376149c509430
timostadelmayer <masztalerztimo@gmail.com>
2024-05-22 11:38:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/WeekFragment.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentWeekBinding
+
+
+class WeekFragment : Fragment() {
+    private lateinit var binding: FragmentWeekBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWeekBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        findNavController().navigate(R.id.appointmentFragment)
+        Log.d("mainFragment", "Speichern Button geklickt!")
+    }
+}
+
